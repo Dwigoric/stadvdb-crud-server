169eedbbcd1f43d83ce72aa03d4d69b138a0089a
Joshua Permito <dwigoric@insanedevs.com>
2024-04-08 11:19:20 +0800

    feat: implement round-robin for replica nodes

--- src/routes/db.js
+++ src/routes/db.js
@@ -1,17 +1,58 @@
 // Package imports
 import { Router } from 'express'
-import { PrismaClient } from '@prisma/client'
+import prismaClientPkg from '@prisma/client'
+
+const { PrismaClient, Prisma: { Sql } } = prismaClientPkg
 
 const router = Router()
 
 // ------ Set up Prisma clients ------
 // Connector for the master node (write operations)
 const master = new PrismaClient({ datasources: { db: { url: process.env.MASTER_URL } } })
-// Connector for Luzon load balancer
+// Connector for Luzon
 const readLuzon = new PrismaClient({ datasources: { db: { url: process.env.LUZON_URL } } })
-// Connector for Vismin load balancer
+// Connector for Vismin
 const readVismin = new PrismaClient({ datasources: { db: { url: process.env.VISMIN_URL } } })
 // Source: https://github.com/prisma/prisma/issues/2443#issuecomment-630679118
+
+let masterAvailable = true
+let luzonNode = readLuzon
+let visminNode = readVismin
+
+const roundRobin = setInterval(async () => {
+    // Check Master
+    try {
+        await master.$queryRaw(Sql`SELECT 1`)
+        // Stay on Master
+        masterAvailable = true
+    } catch (e) {
+        console.error('Master is down')
+        masterAvailable = false
+        // Revert to Luzon and Vismin replicas
+        luzonNode = readLuzon
+        visminNode = readVismin
+    }
+
+    // Check Luzon, fallback to Master
+    try {
+        await luzonNode.$queryRaw(Sql`SELECT 1`)
+        // Stay/switch to Luzon
+        luzonNode = readLuzon
+    } catch (e) {
+        console.error('Luzon is down, falling back to master')
+        luzonNode = master
+    }
+
+    // Check Vismin, fallback to Master
+    try {
+        await visminNode.$queryRaw(Sql`SELECT 1`)
+        // Stay/switch to Vismin
+        visminNode = readVismin
+    } catch (e) {
+        console.error('Vismin is down, falling back to master')
+        visminNode = master
+    }
+}, 10_000) // every 10 seconds
 
 router.get('/', function(req, res, next) {
     res.send('Hello there!')
